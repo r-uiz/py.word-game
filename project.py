536342364bba461cb22d047c3d30d7ba91c7a366
r-uiz <ruizdelcarmen1@gmail.com>
2023-09-20 12:17:05 +0800

    Main functions in place, copy attempt to clipboard, KeyboardInterrupt, reattempt, game end

--- project.py
+++ project.py
@@ -9,15 +9,31 @@ from captcha.image import ImageCaptcha
 
 def main():
     # explanation
+    print("Welcome to Py.word Game! Choose a password.")
+    print("Ctrl+C to quit the program")
     # ask for attempt
-    attempt = input("Password: ")
-    # copy attempt to clipboard
-    subprocess.run("pbcopy", text=True, input=attempt)
-    # try test against rules
-    # loop if invalid
-    # message: invalid. Follow reqs, attempt copied to clipboard, ctrl D if quit program
-    # sys.exit if except catches EOFError (ctrl D)
-    ...
+    try:
+        while True:
+            attempt = input("Password: ")
+            if validate(attempt) == False:
+                subprocess.run("pbcopy", text=True, input=attempt)
+                print("Last attempt copied to clipboard. Ctrl+C to quit.")
+                pass
+            else:
+                sys.exit(
+                    f'Congrats! Password "{attempt}" is valid. Wait... did we say that out loud?'
+                )
+    except KeyboardInterrupt:
+        print("\nProgram quit. Try to create a password again later!")
+
+    # if finally valid, exit
+
+
+def validate(s):
+    # contains all reqs; para prettier main lang
+    # if invalid based on req, return False.
+    # if valid, return True
+    return False
 
 
 def normal_reqs(s):
